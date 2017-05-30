b902c92dabf4ba5bff461a596610ee38eb4a5d74
Nouval Hussein <nouval.hussein@pwc.com>
2017-05-30 16:49:44 -0400

    allow any ip and port 5000

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ namespace dncore_hello
         {
             var host = new WebHostBuilder()
                 .UseKestrel()
+                .UseUrls("http://*:5000")
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
